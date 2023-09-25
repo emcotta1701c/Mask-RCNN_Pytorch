06ea0cc1428f5526aa1477eaf1e616e5cd150e26
emcotta1701c <141988532+emcotta1701c@users.noreply.github.com>
2023-09-24 22:36:08 -0700

    Modified COCO_MODEL_PATH to point to Google drive from Colab.

--- demo.py
+++ demo.py
@@ -26,7 +26,8 @@ MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 # Path to trained weights file
 # Download this file and place in the root of your
 # project (See README file for details)
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
+# COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "drive/MyDrive/Mask-RCNN_Pytorch/mask_rcnn_coco.pth")
 
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
